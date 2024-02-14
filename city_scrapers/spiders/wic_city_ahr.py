4b22b4f57a51296e2f8e38f6535f7e288e58ccc1
Daniel Simmons-Ritchie <37225902+SimmonsRitchie@users.noreply.github.com>
2024-02-14 12:32:41 -0600

    Tweak agency name format

--- city_scrapers/spiders/wic_city_ahr.py
+++ city_scrapers/spiders/wic_city_ahr.py
@@ -3,5 +3,5 @@ from city_scrapers.mixins.wichita_city_mixin import WichitaCityMixin
 
 class WicCityAhrSpider(WichitaCityMixin):
     name = "wic_city_ahr"
-    agency = "City of Wichita - Affordable Housing Review Board"
+    agency = "Wichita City - Affordable Housing Review Board"
     cid = 30
--- city_scrapers/spiders/wic_city_apc.py
+++ city_scrapers/spiders/wic_city_apc.py
@@ -3,5 +3,5 @@ from city_scrapers.mixins.wichita_city_mixin import WichitaCityMixin
 
 class WicCityApcSpider(WichitaCityMixin):
     name = "wic_city_apc"
-    agency = "City of Wichita - Advance Plans Committee"
+    agency = "Wichita City - Advance Plans Committee"
     cid = 68
